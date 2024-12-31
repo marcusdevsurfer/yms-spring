a4311530ee82f4c93a62bbea203d97c0cce7ef3e
marcusdevsurfer <marcus.sea13@gmail.com>
2024-12-31 15:20:42 -0600

    container repository added

--- src/main/java/com/dev/demo/repository/ContainerRepository.java
+++ src/main/java/com/dev/demo/repository/ContainerRepository.java
@@ -1,6 +1,8 @@
 package com.dev.demo.repository;
 import com.dev.demo.entity.Container;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContainerRepository extends MongoRepository<Container, String> {
 }
